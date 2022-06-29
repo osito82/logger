2e7ac8829bfebb2c36a66559762bb2236f9b90db
osito82 <info@guillermosalazar.net>
2022-06-29 12:52:27 -0400

    implementing colors

--- functions/string.js
+++ functions/string.js
@@ -1,12 +1,26 @@
-import * as misc from '../lib/misc.js'
+import * as misc from "../lib/misc.js";
+import colors from "colors";
+import _ from "lodash";
 
-//checking if it is a string
-const stringConsole = (obj, config, i) => {
+const stringConsole = (obj, config, i, logArguments) => {
+  if (logArguments.colors != "default") {
+    _.assign(logArguments, { colors: logArguments.colors });
+    
+    colors.setTheme({
+      custom: logArguments.colors
+    });
+  }
+  else {
+    colors.setTheme({
+      custom: 'reset'
+    });
+  }
+
   if (misc.countNumberKeys(obj) >= 1) {
     if (config.string_caps == false) {
-      console.log(obj[i]);
+      console.log(obj[i].custom);
     } else if (config.string_caps == true) {
-      console.log(misc.toUpperCaps(obj[i]));
+      console.log(misc.toUpperCaps(obj[i].custom));
     }
   }
 };
